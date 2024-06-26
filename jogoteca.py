272883b6632b27b114fa0133b11d080211ecebe5
Daniel Souza <workdndsza@gmail.com>
2024-06-26 15:14:00 -0300

    criando o template e adicionando dinamicamente

--- jogoteca.py
+++ jogoteca.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Olá, mundo!'
+    jogos = ['Super Mario', 'Zelda', 'Pokemon', 'Donkey Kong', 'Mortal Kombat', 'Street Fighter', 'Final Fantasy', 'Castlevania', 'Metroid', 'Sonic']
+    return render_template('lista.html', titulo='Jogos', jogos=jogos)
 
 
 app.run(debug=True)
