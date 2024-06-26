2dd73d88b3ef24b4ebff49dddc7f704b71bed233
Daniel Souza <workdndsza@gmail.com>
2024-06-26 18:03:28 -0300

    terminando o crud dos jogos

--- views/views.py
+++ views/views.py
@@ -4,11 +4,17 @@ from jogoteca import app, db
 
 @app.route('/')
 def index():
+    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+        return redirect(url_for('login'))
+
     jogos = Jogos.query.order_by(Jogos.categoria).all()
     return render_template('lista.html', titulo='Jogos', jogos=jogos)
 
 @app.route('/novo')
 def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+        return redirect(url_for('login'))
+
     return render_template('novo.html', titulo='Novo Jogo')
 
 @app.route('/criar', methods=['POST',])
@@ -50,3 +56,34 @@ def logout():
     session['usuario_logado'] = None
     flash('Logout realizado com sucesso!')
     return redirect(url_for('login'))
+
+@app.route('/editar/<int:id>')
+def editar(id):
+    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+        return redirect(url_for('login'))
+
+    jogo = Jogos.query.filter_by(id=id).first()
+
+    return render_template('editar.html', titulo='Editando Jogo', jogo=jogo)
+
+@app.route('/atualizar', methods=['POST',])
+def atualizar():
+    id = request.form['id']
+    jogo = Jogos.query.filter_by(id=id).first()
+    jogo.nome = request.form['nome']
+    jogo.categoria = request.form['categoria']
+    jogo.console = request.form['console']
+    db.session.commit()
+    flash(f'O jogo {jogo.nome} foi atualizado com sucesso!')
+    return redirect(url_for('index'))
+
+@app.route('/deletar/<int:id>')
+def deletar(id):
+    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+        return redirect(url_for('login'))
+
+    jogo = Jogos.query.filter_by(id=id).first()
+    db.session.delete(jogo)
+    db.session.commit()
+    flash(f'O jogo {jogo.nome} foi deletado com sucesso!')
+    return redirect(url_for('index'))
