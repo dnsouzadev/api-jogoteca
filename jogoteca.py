613b59e4eeac23f78b34916a244ffc94d2df2a41
Daniel Souza <workdndsza@gmail.com>
2024-06-26 15:32:10 -0300

    criando as rotas

--- jogoteca.py
+++ jogoteca.py
@@ -1,11 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
+jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'SNES')
+jogo4 = Jogo('GTA V', 'Ação', 'PS4')
+jogos = [jogo1, jogo2, jogo3, jogo4]
+
+
 @app.route('/')
 def index():
-    jogos = ['Super Mario', 'Zelda', 'Pokemon', 'Donkey Kong', 'Mortal Kombat', 'Street Fighter', 'Final Fantasy', 'Castlevania', 'Metroid', 'Sonic']
     return render_template('lista.html', titulo='Jogos', jogos=jogos)
 
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    jogos.append(jogo)
+    return redirect(url_for('index'))
 
 app.run(debug=True)
