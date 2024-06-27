1c60e05badd8b58b3e6d040b779f7519e3f6bd35
Daniel Souza <workdndsza@gmail.com>
2024-06-27 16:16:17 -0300

    encryptando a senha com o bcrypt

--- views/views_users.py
+++ views/views_users.py
@@ -2,6 +2,7 @@ from flask import flash, redirect, render_template, request, url_for, session
 from models.Usuarios import Usuarios
 from jogoteca import app
 from models.forms import FormLogin
+from flask_bcrypt import check_password_hash
 
 @app.route('/login')
 def login():
@@ -13,15 +14,19 @@ def autenticar():
     form = FormLogin(request.form)
 
     usuario = Usuarios.query.filter_by(nickname=form.nickname.data).first()
+    senha = check_password_hash(usuario.senha, form.senha.data)
     if not usuario:
         flash('Usuário não encontrado!')
         return redirect(url_for('login'))
-    if usuario.senha != form.senha.data:
+    if not senha:
         flash('Senha incorreta!')
         return redirect(url_for('login'))
-    session['usuario_logado'] = usuario.nickname
-    flash(f'{usuario.nome} logou com sucesso!')
-    return redirect(url_for('index'))
+    if usuario and senha:
+        session['usuario_logado'] = usuario.nickname
+        flash(f'{usuario.nome} logou com sucesso!')
+        return redirect(url_for('index'))
+    else:
+        flash('Dados incorretos!')
 
 @app.route('/logout')
 def logout():
